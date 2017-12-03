a5b5f350c012e86db498993665ef5488795d9c6b
Peter Partridge <petertoby@aol.com>
2017-12-03 13:42:47 +0000

    adding NHibernate to Business layer and adding a the tested generic call along with session factory and creating inheritance between the two.

---
+++ BusinessLayer/Factories/UserCompanyVehicleFactory.cs
@@ -0,0 +1,33 @@
+﻿using DataLayer.Class;
+using DataLayer.Mapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Criterion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Factories
+{
+   public  class UserCompanyVehicleFactory
+    {
+       public static ISessionFactory _sessionFactory;
+
+
+        public void BuildCompanyUserVehicleQueryFactory()
+        {
+            var config = Fluently.Configure()
+             .Database(
+              MsSqlConfiguration.MsSql2008.ConnectionString(c => c.FromConnectionStringWithKey("Default")).ShowSql())
+              .Mappings(
+              m => m.FluentMappings.AddFromAssemblyOf<CompanyMap>().AddFromAssemblyOf<User>().AddFromAssemblyOf<Vehicle>()
+            ).BuildConfiguration();
+
+
+            _sessionFactory = config.BuildSessionFactory();
+        }
+    }
+}
---
+++ BusinessLayer/GenericDBCalls/GetListOfObjects.cs
@@ -0,0 +1,48 @@
+﻿using BusinessLayer.Factories;
+using DataLayer.Class;
+using DataLayer.Mapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Criterion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.GenericDBCalls
+{
+  public class GetListOfObjects : UserCompanyVehicleFactory
+    {
+
+        public IList<T> GetGenericItem<T>(string propertyName, object searchparam)
+        {
+            BuildCompanyUserVehicleQueryFactory();
+
+            IList<T> CompanyListResult = new List<T>();
+            using (ISession session = _sessionFactory.OpenSession())
+            {
+                using (var transaction = session.BeginTransaction())
+                {
+                    if (searchparam is string)
+                    {
+                        searchparam = "%" + searchparam + "%";
+
+                        CompanyListResult = session.CreateCriteria(typeof(T)).Add(Restrictions.InsensitiveLike(propertyName, searchparam)).List<T>();
+                    }
+                    else
+                    {
+                        CompanyListResult = session.CreateCriteria(typeof(T)).Add(Restrictions.Gt(propertyName, searchparam) || Restrictions.Eq(propertyName, searchparam)).List<T>();
+                    }
+                }
+            }
+
+            return CompanyListResult;
+
+        }
+
+      
+
+    }
+}
